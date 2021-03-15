26640f83869f1833c39958a30fac0d64e92f9b1f
Nikita <doofeee@gmail.com>
2021-03-15 02:00:29 +0200

    made HW3

---
+++ Coding/Class.Constructor/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task3
+{
+
+
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer(1,"Benjamin","Amazon");
+            customer1.PrintInfo();
+            customer1.Article = "Mouse"; // товар - компьютерная мышка
+            customer1.Quantity = 5; // 5 шт. количество
+
+            customer1.CalcNds(300); // выводит инфу о прайсе с НДС и без НДС , передаем в метод стоимость за одну единицу товара
+        }
+    }
+}
---
+++ Coding/justCoding/justCoding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace justCoding
+{
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public Person() : this("Неизвестно")
+        {
+        }
+        public Person(string name) : this(name, 18)
+        {
+        }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person tom = new Person { name = "Tom", age = 31 };
+            tom.GetInfo();          // Имя: Tom  Возраст: 31
+        }
+    }
+}
