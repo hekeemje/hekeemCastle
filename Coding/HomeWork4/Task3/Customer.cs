6fe22f9bb0b016d5afff08a6e685d2708134809a
Nikita <doofeee@gmail.com>
2021-03-17 22:27:38 +0200

    changes

---
+++ Coding/HomeWork4/Task3/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Task3
+{
+    class Customer
+    {
+        private readonly int account;
+        private readonly string customer;
+        private readonly string provider;
+        private string article;
+        private int quantity;
+
+        public string Article
+        {
+            get { return article; }
+            set { article = value; }
+        }
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"account = {account} | customer = {customer} | provider = {provider}");
+        }
+
+        public Customer(int account , string customer, string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+
+        public void CalcNds(int price)
+        {
+            int withoutnds = price * quantity;
+            double withnds = withoutnds * 0.2 + withoutnds; // если НДС 20%
+
+            Console.WriteLine($"Price with NDS : {withnds} || Price without NDS {withoutnds}");
+        }
+
+    }
+}
--- Coding/justCoding/justCoding/Program.cs
+++ Coding/justCoding/justCoding/Program.cs
@@ -4,32 +4,42 @@ namespace justCoding
 {
     class Person
     {
-        public string name;
-        public int age;
+        private int lol;
 
-        public Person() : this("Неизвестно")
-        {
-        }
-        public Person(string name) : this(name, 18)
-        {
-        }
-        public Person(string name, int age)
-        {
-            this.name = name;
-            this.age = age;
-        }
-        public void GetInfo()
-        {
-            Console.WriteLine($"Имя: {name}  Возраст: {age}");
-        }
+        public int Lol { get { return lol; }
+            set { lol = value; } }
+        //public string name;
+        //public int age;
+
+        //public Person() : this("Неизвестно")
+        //{
+        //}
+        //public Person(string name) : this(name, 18)
+        //{
+        //}
+        //public Person(string name, int age)
+        //{
+        //    this.name = name;
+        //    this.age = age;
+        //}
+        //public void GetInfo()
+        //{
+        //    Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        //}
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Person tom = new Person { name = "Tom", age = 31 };
-            tom.GetInfo();          // Имя: Tom  Возраст: 31
+            //Person tom = new Person { name = "Tom", age = 31 };
+            //tom.GetInfo();          // Имя: Tom  Возраст: 31
+
+            Person[] cs = new Person[10];
+            cs[0].Lol = 5;
+
+            Console.WriteLine(cs[0].Lol);
+
         }
     }
 }
