81ddded02f07aef5d0222698e0e7ea61b9969278
Nikita <doofeee@gmail.com>
2021-04-20 02:02:35 +0300

    1

--- Coding/justCoding/justCoding/Program.cs
+++ Coding/justCoding/justCoding/Program.cs
@@ -13,6 +13,26 @@ namespace justCoding
             b = (U)(object)temp;
         }
     }
+
+    class MyClass
+    {
+        public int field;
+
+        public MyClass(int n)
+        {
+            field = n;
+        }
+
+        public override string ToString()
+        {
+            return $"Field = {field}";
+        }
+
+        public static implicit operator MyClass(int n)
+        {
+            return new MyClass(n);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -24,6 +44,14 @@ namespace justCoding
             Console.WriteLine($"Before : a = {a} and b = {b}");
             util.SwapGenericObjects<int, int>(ref a, ref b);
             Console.WriteLine($"After : a = {a} and b = {b}");
+
+            Show<MyClass>(400);
+        }
+
+        public static void Show<T>(T text)
+        {
+            Console.WriteLine(typeof(T).Name);
+            Console.WriteLine(text);
         }
     }
 }
