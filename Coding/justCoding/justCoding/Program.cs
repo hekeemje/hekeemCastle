183fed10d743e4592e3b5d835197e882c204991f
Nikita <doofeee@gmail.com>
2021-05-19 18:12:36 +0300

    22

--- Coding/justCoding/justCoding/Program.cs
+++ Coding/justCoding/justCoding/Program.cs
@@ -4,11 +4,26 @@ using System.Linq;
 
 namespace justCoding
 {
+
     class Program
     {
+        static void F4(Dictionary<int, string> keyValuePairs)
+        { 
+            foreach (var item in keyValuePairs)
+                Console.WriteLine($"{item.Key} : {item.Value}");
+        }
+
+        public delegate void MyDicDel(Dictionary<int, string> keks);
+
         static void Main(string[] args)
         {
-            Console.WriteLine(9%2);
+            Dictionary<int, string> kk1 = new Dictionary<int, string>();
+            kk1.Add(1, "one");
+            kk1.Add(2, "two");
+            kk1.Add(3, "three");
+
+            MyDicDel myDicDel = F4;
+            myDicDel.Invoke(kk1);
         }
 
     }
