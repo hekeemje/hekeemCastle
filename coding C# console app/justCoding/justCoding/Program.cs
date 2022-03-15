e882ec2c83751cbf18723168272eae8fdcc927d1
Nikita <doofeee@gmail.com>
2022-03-15 17:48:40 +0200

    changes

--- coding C# console app/justCoding/justCoding/Program.cs
+++ coding C# console app/justCoding/justCoding/Program.cs
@@ -7,24 +7,58 @@ namespace justCoding
 
     class Program
     {
-        static void F4(Dictionary<int, string> keyValuePairs)
-        { 
-            foreach (var item in keyValuePairs)
-                Console.WriteLine($"{item.Key} : {item.Value}");
-        }
-
-        public delegate void MyDicDel(Dictionary<int, string> keks);
-
         static void Main(string[] args)
         {
-            Dictionary<int, string> kk1 = new Dictionary<int, string>();
-            kk1.Add(1, "one");
-            kk1.Add(2, "two");
-            kk1.Add(3, "three");
+            int[] nums = { 2, -7, 3, 5, -1, 1, 4, -5, 9, -2 };
 
-            MyDicDel myDicDel = F4;
-            myDicDel.Invoke(kk1);
+            IEnumerable<int> negative =
+                         from num in nums
+                         where num < 0
+                         select num;
+
+            foreach (var item in negative)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            IEnumerable<int> positive =
+                        from num in nums
+                        where num >= 0
+                        select num;
+
+            foreach (var item in positive)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            var min = nums.Min();
+            Console.WriteLine($"Min : {min}");
+
+            var max = nums.Max();
+            Console.WriteLine($"Max : {max}");
+
+            var sum = nums.Sum();
+            Console.WriteLine($"Sum : {sum}");
+
+            var avg = nums.Average();
+            Console.WriteLine($"Average : {avg}");
+            var where = nums.Where(p => p <= avg).Max();
+            Console.WriteLine($"First Large : {where}");
+
+            var first = nums.First();
+
+            var orderedNumbers = from num in nums
+                                 orderby num
+                                 select num;
+
+            foreach (var item in orderedNumbers)
+            {
+                Console.WriteLine(item);
+            }
         }
-
     }
 }
